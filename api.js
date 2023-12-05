d0455165200b998edabab183dbc2ce1a9c7f7249
duffblackraven <t9164700668@gmail.com>
2023-12-05 14:54:25 +0100

    Instapro project

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-// Замени на свой, чтобы получить независимый от других набор данных.
-// "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+import { setPosts } from "./index.js";
+
+const personalKey = "tatiana-ret";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
@@ -23,7 +23,7 @@ export function getPosts({ token }) {
     });
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
@@ -33,7 +33,8 @@ export function registerUser({ login, password, name, imageUrl }) {
       name,
       imageUrl,
     }),
-  }).then((response) => {
+  })
+  .then((response) => {
     if (response.status === 400) {
       throw new Error("Такой пользователь уже существует");
     }
@@ -48,7 +49,8 @@ export function loginUser({ login, password }) {
       login,
       password,
     }),
-  }).then((response) => {
+  })
+  .then((response) => {
     if (response.status === 400) {
       throw new Error("Неверный логин или пароль");
     }
@@ -56,7 +58,6 @@ export function loginUser({ login, password }) {
   });
 }
 
-// Загружает картинку в облако, возвращает url загруженной картинки
 export function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
@@ -64,7 +65,86 @@ export function uploadImage({ file }) {
   return fetch(baseHost + "/api/upload/image", {
     method: "POST",
     body: data,
-  }).then((response) => {
+  })
+  .then((response) => {
     return response.json();
   });
+}
+
+export function addPost ({ token, imageUrl }) {
+  const commInputElement = document.getElementById('description')
+  return fetch(postsHost, {
+    method: 'POST',
+    body: JSON.stringify({
+        description: commInputElement.value,
+        imageUrl,
+    }),
+    headers: {
+        Authorization: token,
+    },
+ })
+.then((response) => {
+    if (response.status === 400) {
+        alert('Нет фото или описания')
+    } else {
+        return response.json()
+    }
+ })
+}
+
+export function getUserPost ({ token, userId}) {
+  return fetch(`${postsHost}/user-posts/${userId}`, {
+    method: 'GET',
+    headers: {
+        Authorization: token,
+    },
+})
+    .then((response) => {
+        if (response.status === 401) {
+            throw new Error('Нет авторизации')
+        }
+        return response.json()
+    })
+    .then((data) => {
+        setPosts(data.posts)
+        return data.posts
+    })
+    .catch((error) => {
+        alert('Кажется, у вас сломался интернет, попробуйте позже')
+        console.warn(error)
+    })
+}
+
+export function addLike ({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/like`, {
+    method: 'POST',
+    headers: {
+        Authorization: token,
+    },
+ })
+.then((response) => {
+    if (response.status === 401) {
+        alert('Лайкать посты могут только авторизованные пользователи')
+        throw new Error('Нет авторизации')
+    }
+
+    return response.json()
+ })
+}
+
+export function removeLike ({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/dislike`, {
+    method: 'POST',
+    headers: {
+        Authorization: token,
+    },
+ })
+.then((response) => {
+    if (response.status === 401) {
+        alert('Чтобы убрать лайк, войдите')
+        throw new Error('Нет авторизации')
+    }
+
+    return response.json()
+ })
 }
